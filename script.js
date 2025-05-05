61bf5a104211c2dc84e795cf36392850c0d08c12
Nexus2288 <osvpriyanshu09@gmail.com>
2025-05-05 11:20:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openTelegram() {
+  window.location.href = "https://t.me/your_username";
+}
+
+function openInstagram() {
+  window.location.href = "https://instagram.com/your_username";
+}
